191f7795e6a8da39e0e73f04f2b8c9a0f4134ded
cohae <cohae@cohae.dev>
2026-05-09 20:34:16 +0200

    feat: Proper RayCastResult type

--- src/physics/narrow_phase.rs
+++ src/physics/narrow_phase.rs
@@ -2,11 +2,17 @@ use std::{marker::PhantomData, mem::MaybeUninit};
 
 use mint::{Point3, Vector3};
 
-use crate::{Real, Vec3Ext};
+use crate::{BodyId, Real, SubShapeId, Vec3Ext};
 
 pub struct Ray {
     origin: Point3<Real>,
     direction: Vector3<f32>,
+}
+
+pub struct RayCastResult {
+    pub body_id: BodyId,
+    pub fraction: f32,
+    pub sub_shape_id: SubShapeId,
 }
 
 pub struct NarrowPhaseQuery<'a>(*const jolt_sys::JPC_NarrowPhaseQuery, PhantomData<&'a ()>);
@@ -18,7 +24,7 @@ impl<'a> From<*const jolt_sys::JPC_NarrowPhaseQuery> for NarrowPhaseQuery<'a> {
 }
 
 impl<'a> NarrowPhaseQuery<'a> {
-    pub fn cast_ray(&self, ray: &Ray) -> Option<jolt_sys::JPC_RayCastResult> {
+    pub fn cast_ray(&self, ray: &Ray) -> Option<RayCastResult> {
         let ray = jolt_sys::JPC_RRayCast {
             origin: ray.origin.to_fixed_vec3(),
             direction: ray.direction.to_fixed_vec3(),
@@ -37,6 +43,13 @@ impl<'a> NarrowPhaseQuery<'a> {
             )
         };
 
-        hit.then_some(unsafe { result.assume_init() })
+        hit.then_some({
+            let raw_result = unsafe { result.assume_init() };
+            RayCastResult {
+                body_id: BodyId(raw_result.body_id),
+                fraction: raw_result.fraction,
+                sub_shape_id: SubShapeId(raw_result.sub_shape_id),
+            }
+        })
     }
 }
